275c9a9f7e9ce2ae1757b7f56077869d356594c9
Touko <touko.nurminen@gmail.com>
2024-08-26 23:15:22 +0300

    Change remaps, install neogit and color scheme

--- lua/toukka/plugin_config/telescope.lua
+++ lua/toukka/plugin_config/telescope.lua
@@ -8,8 +8,8 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>s', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>f', function()
+        vim.keymap.set('n', '<leader>p', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
     end
--- lua/toukka/set.lua
+++ lua/toukka/set.lua
@@ -1,3 +1,5 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.signcolumn = "number"
+
+vim.opt.tabstop = 4

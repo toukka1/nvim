da069170826d414623c1ff2d2132ab9179bc98b9
Touko <touko.nurminen@gmail.com>
2024-08-27 19:50:32 +0300

    Fix telescope wrong remap

--- lua/toukka/plugin_config/telescope.lua
+++ lua/toukka/plugin_config/telescope.lua
@@ -9,7 +9,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>s', builtin.find_files, {})
         vim.keymap.set('n', '<leader>p', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>', function()
+        vim.keymap.set('n', '<leader>f', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
     end

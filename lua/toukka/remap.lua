727ea6b8188d27d56e8052c275718a4e342425a5
Touko <touko.nurminen@gmail.com>
2024-08-27 21:44:44 +0300

    Add clipboard support

--- lua/toukka/remap.lua
+++ lua/toukka/remap.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("i", "<leader>q", "<Esc>")
--- lua/toukka/set.lua
+++ lua/toukka/set.lua
@@ -6,3 +6,5 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.opt.clipboard:append("unnamedplus")

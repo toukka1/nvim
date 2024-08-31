45f85a286ffd67e0dd0df2ac24f811179f35a6dc
Touko <touko.nurminen@gmail.com>
2024-08-31 13:03:21 +0300

    change broken remap

--- lua/toukka/remap.lua
+++ lua/toukka/remap.lua
@@ -4,4 +4,4 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("i", "<leader>q", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")

777d7f292d2be07c3fb6721cc799f0c5c02c7ba8
Touko <touko.nurminen@gmail.com>
2024-09-10 20:59:43 +0300

    more remaps

--- lua/toukka/remap.lua
+++ lua/toukka/remap.lua
@@ -1,7 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- preserve pasted text
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")

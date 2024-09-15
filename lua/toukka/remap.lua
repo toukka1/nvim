4ea1d984a7ccf3097b81c256705be7679758d469
Touko Nurminen <touko.nurminen@aalto.fi>
2024-09-15 19:33:38 +0300

    Remap umlauts to brackets and braces

--- lua/toukka/remap.lua
+++ lua/toukka/remap.lua
@@ -11,3 +11,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- umlauts to braces and brackets
+vim.keymap.set("i", "ö", "[")
+vim.keymap.set("i", "ä", "]")
+vim.keymap.set("i", "Ö", "{")
+vim.keymap.set("i", "Ä", "}")

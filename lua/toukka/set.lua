3f2829fa6b19ab75a492d44a9221eed7f300f6a9
Touko Nurminen <touko.nurminen@aidian.eu>
2024-10-25 11:08:46 +0300

    set guicursor, backup and undo

--- lua/toukka/set.lua
+++ lua/toukka/set.lua
@@ -1,3 +1,5 @@
+vim.opt.guicursor = ""
+
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.signcolumn = "number"
@@ -7,6 +9,13 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.wrap = false
 

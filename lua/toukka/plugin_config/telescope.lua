39e1e73d39632023eb7fdce84bb04104d1503364
Touko <touko.nurminen@gmail.com>
2024-11-20 19:13:50 +0200

    Telescope live grep

--- lua/toukka/plugin_config/telescope.lua
+++ lua/toukka/plugin_config/telescope.lua
@@ -9,8 +9,6 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>s', builtin.find_files, {})
         vim.keymap.set('n', '<leader>p', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>f', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
     end
 }

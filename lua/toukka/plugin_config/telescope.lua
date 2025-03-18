d9291b6dea2225b3a92a0d9dde41585dc5a83f87
Touko Nurminen <touko.nurminen@aidian.eu>
2025-03-18 11:49:06 +0200

    Update keybinds
    Add live grep with selected text

--- lua/toukka/plugin_config/telescope.lua
+++ lua/toukka/plugin_config/telescope.lua
@@ -7,8 +7,10 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>s', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>p', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+        vim.keymap.set('n', '<C-s>', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+        -- Live grep selected text
+        vim.keymap.set('v', '<C-f>', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', {})
     end
 }

db4d0dbee114a511cee39ec956f736357f0b7bd0
Touko Nurminen <touko.nurminen@aidian.eu>
2025-10-28 10:33:30 +0200

    Remove copilot

--- init.lua
+++ init.lua
@@ -49,7 +49,6 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 -- PLUGINS
 vim.pack.add({
     { src = "https://github.com/catppuccin/nvim" },
-    { src = "https://github.com/github/copilot.vim" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/nvim-telescope/telescope.nvim" },

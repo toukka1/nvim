246ebc0d59517e1460f1ab4a5e6286e19efad999
Touko Nurminen <touko.nurminen@gmail.com>
2026-01-10 12:48:04 +0200

    Add back exit remap

--- init.lua
+++ init.lua
@@ -41,6 +41,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Preserve pasted text
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Put cut out characters in another register
 vim.keymap.set("n", "x", '"_x')
